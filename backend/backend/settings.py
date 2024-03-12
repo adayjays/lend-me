de4388d048f81aa3264f726bb2793d8f3de76a5e
adayjays <adajiahtwatson@gmail.com>
2024-03-12 17:09:42 +0300

    seting things up for render deployment #4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(hbadc+-(h=xrlb)7(pr#d24*yn7u68&6--*numqu85n(ollfk'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
