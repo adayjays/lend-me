865f61aff7d792c582058621a1ecdbdff4f6122e
adayjays <adajiahtwatson@gmail.com>
2024-03-12 17:05:45 +0300

    seting things up for render deployment #3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,9 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(hbadc+-(h=xrlb)7(pr#d24*yn7u68&6--*numqu85n(ollfk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lend-me.onrender.com']
 
 
 # Application definition
